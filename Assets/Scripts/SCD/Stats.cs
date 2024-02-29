2c5386c137059d70c4e3dc41a086552859b86390
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-02-29 21:05:53 +0900

    Fix Stats class dictionary initialization bug

--- Assets/Scripts/SCD/Stats.cs
+++ Assets/Scripts/SCD/Stats.cs
@@ -8,58 +8,64 @@ namespace SCD
     /// </summary>
     public sealed class Stats
     {
-        private readonly Dictionary<string, Record> records = new ();
+        private readonly Dictionary<string, Record> records = new();
 
         /// <summary>
         /// 変更があった際に呼び出されるイベント
         /// </summary>
         public event Action<Record> OnChanged;
-    
+
         /// <summary>
         /// 統計データを設定する
         /// </summary>
         public void Set(string name, int value)
         {
-            if(records.TryGetValue(name, out var record))
+            if (records.TryGetValue(name, out var record))
             {
-                record.value = value;
+                record.Value = value;
             }
             else
             {
-                records.Add(name, new Record { name = name, value = value });
+                records.Add(name, new Record(name, value));
             }
             OnChanged?.Invoke(record);
         }
-    
+
         /// <summary>
         /// 統計データを加算する
         /// </summary>
         public void Add(string name, int value)
         {
-            if(records.TryGetValue(name, out var record))
+            if (records.TryGetValue(name, out var record))
             {
-                record.value += value;
+                record.Value += value;
             }
             else
             {
-                records.Add(name, new Record { name = name, value = value });
+                records.Add(name, new Record(name, value));
             }
             OnChanged?.Invoke(record);
         }
-        
+
         /// <summary>
         /// 統計データを取得する
         /// </summary>
         public int Get(string name)
         {
-            return records.TryGetValue(name, out var record) ? record.value : 0;
+            return records.TryGetValue(name, out var record) ? record.Value : 0;
         }
-    
+
         public class Record
         {
-            public string name;
+            public string Name { get; }
 
-            public int value;
+            public int Value { get; set; }
+
+            public Record(string name, int value)
+            {
+                Name = name;
+                Value = value;
+            }
         }
     }
 }
